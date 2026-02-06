a188d17ccc21ff6e3d214bcebe1322fbba137344
David Ferreira <davidferreira21@hotmail.com>
2026-02-05 23:05:21 -0300

    eks

--- plataforma/tf-modules/eks/backend.tf
+++ plataforma/tf-modules/eks/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "tfstate-terraform-lab-plataform-engineering"
+    bucket       = "tfstate-814436217857-us-east-1"
     key          = "eks/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
